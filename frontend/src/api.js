9c1744c8dcac7b82ffd9c738b95b0f587dd2bd7a
Nikhil Pandey <pandeynikhilone@gmail.com>
2025-08-19 18:54:39 +0530

    +1 renderURL in api.js | 19-08-2025

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://internshipterms-iii.onrender.com", 
+  baseURL: "https://internshipterms-iii.onrender.com/api", 
 });
 
 export default api;
